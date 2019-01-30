7413822e568379bb6c20f35d01767a90961528d6
lbj <2105931798@qq.com>
2019-01-30 21:06:41 +0800

    add update

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
             Edit <code>src/App.js</code> and save to reload.
             Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
