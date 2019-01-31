84732d91da02f2d8cd8c2e4a4857586bb9a8e8b3
lbj <2105931798@qq.com>
2019-01-31 22:13:58 +0800

    ant quick start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { LocaleProvider, DatePicker, message} from 'antd';
 import './App.css';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
+import 'moment/locale/zh-cn';
+import moment from 'moment';
+import "antd/dist/antd.css";
+import "./index.css";
 
+moment.locale('zh-cn');
 class App extends Component {
+  state = {
+    date: null,
+  };
+
+  handleChange = (date) => {
+    message.info(`您选择的日期是: ${date.format('YYYY-MM-DD')}`);
+    this.setState({ date });
+  }
+
   render() {
+    const { date } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -15,6 +32,14 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <LocaleProvider locale={zhCN}>
+        <div style={{ width: 400, margin: '100px auto' }}>
+          <DatePicker onChange={this.handleChange} />
+          <div style={{ marginTop: 20 }}>
+            当前日期：{date ? date.format('YYYY-MM-DD') : '未选择'}
+          </div>
+        </div>
+      </LocaleProvider>
           <a
             className="App-link"
             href="https://reactjs.org"
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<DatePicker />, document.getElementById('date'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
